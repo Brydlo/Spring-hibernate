ac99b3328c6578baabb33bfb04eabfc3cb53c12e
Tomasz Czerniewski <brydlo2.tc@gmail.com>
2023-10-20 13:45:01 +0200

    first steps

---
+++ src/main/java/sklep/SklepSpringApplication.java
@@ -0,0 +1,13 @@
+package sklep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SklepSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SklepSpringApplication.class, args);
+    }
+
+}
---
+++ src/main/java/sklep/controller/RootController.java
@@ -0,0 +1,15 @@
+package sklep.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class RootController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello world";
+    }
+}

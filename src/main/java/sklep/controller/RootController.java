b9b1b2aca45fb710ef6772fcfeb912bf7e9f98b6
Tomasz Czerniewski <brydlo2.tc@gmail.com>
2023-10-20 14:21:43 +0200

    update pom file

--- src/main/java/sklep/controller/RootController.java
+++ src/main/java/sklep/controller/RootController.java
@@ -1,8 +1,12 @@
 package sklep.controller;
 
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class RootController {
@@ -11,5 +15,11 @@ public class RootController {
     @ResponseBody
     public String hello() {
         return "Hello world";
+    }
+    @GetMapping("/godzina")
+    public String ktoraGodzina(Model model) {
+        LocalDateTime  localDateTimeNow = LocalDateTime.now();
+        model.addAttribute("dt", localDateTimeNow);
+        return "pokaz_czas.jsp";
     }
 }
---
+++ src/main/webapp/pokaz_czas.jsp
@@ -0,0 +1,11 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Pokaż czas</title>
+</head>
+<body>
+<div>Bieżący czas: <strong>${dt}</strong></div>
+</body>
+</html>

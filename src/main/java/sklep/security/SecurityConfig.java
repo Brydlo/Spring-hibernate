acef4f1732f6345fdcadb7cc1894a1c5e7d67589
Tomasz Czerniewski <brydlo2.tc@gmail.com>
2023-11-07 12:30:57 +0100

    change in SecurityConfig

--- src/main/java/sklep/security/SecurityConfig.java
+++ src/main/java/sklep/security/SecurityConfig.java
@@ -1,10 +1,12 @@
 package sklep.security;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,10 +15,27 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain configHttpSecurity(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-                .authorizeHttpRequests(authz -> authz.anyRequest().permitAll());
-//                .csrf(AbstractHttpConfigurer::disable);
+                .authorizeHttpRequests(authz -> authz.anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults())
+//   		 .csrf(authz -> authz.disable())
+        ;
 
         return httpSecurity.build();
-
     }
+
+
+    // Aspektem konfiguracji, który jest podawany w innej metodzie, jest zdefiniowany zbiór użytkowników.
+    // W tej wersji definiujemy użytkowników w kodzie aplikacji ("in memory").
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails[] users = {
+                User.withUsername("ala").password("{noop}ala123").roles("manager", "worker").build(),
+                User.withUsername("ola").password("{noop}ola123").roles("worker").build(),
+        };
+        return new InMemoryUserDetailsManager(users);
+    }
+
+
 }
+
+
